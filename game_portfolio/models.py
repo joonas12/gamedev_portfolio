8c735ec8c74fc8621dae7ca77068c27e0cee0d33
Joonas <skeleton.hunter64@gmail.com>
2023-12-07 12:33:31 +0200

    added html and css files

--- game_portfolio/models.py
+++ game_portfolio/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Testi(models.Model):
     description = models.CharField(max_length=8)
-
+    date_added = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.description
--- game_portfolio/views.py
+++ game_portfolio/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Testi
 
-# Create your views here.
+def index(request):
+    """The home page for Travel Journals."""
+    return render(request, 'game_portfolio/index.html')
+
+def tests(request):
+    """show all Testis"""
+    tests = Testi.objects.order_by('date_added')
+    context = {'tests':tests}
+    return render(request, 'game_portfolio/tests.html', context)
